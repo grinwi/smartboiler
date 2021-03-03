a502dbf7796946ca9a9dca11ea18f0b4e5fc4f2e
grinwi <grunwald.adam24gmail.com>
2021-03-03 12:12:57 +0100

    event checker try except to service

--- control/event_checker.py
+++ control/event_checker.py
@@ -51,6 +51,7 @@ class EventChecker:
             service = build('calendar', 'v3', credentials=creds)
         except:
             print("couldnt build service")
+            return False
         # Call the Calendar API
         now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
         events_result = service.events().list(calendarId='primary', timeMin=now,
