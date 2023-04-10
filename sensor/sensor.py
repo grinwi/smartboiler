ba3c33d628c5461f0bdc0a694f3fc6cc164c3847
grinwi <grunwald.adam24@gmail.com>
2023-04-10 10:03:45 +0300

    added shelly and udpated db api

--- sensor/sensor.py
+++ sensor/sensor.py
@@ -8,115 +8,107 @@
 # Module that collects data from a smart socket.          #
 ###########################################################
 
-from influxdb import InfluxDBClient
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
 import requests
-
-import datetime
-import time
-import json
-
-import math
-import pprint
-import os
-import signal
-import sys
-from settings_loader import SettingsLoader
-from event_checker import EventChecker
-
-EvntChecker = EventChecker()
-
-
 client = None
+import os
+import sys
+import math
+import json
+import time
+import pprint
+import signal
+import datetime
+from shelly import SmartBoiler
+from optparse import OptionParser
+from settings_loader import SettingsLoader
 
-def db_exists():
-    """Returns True if the database exists.
+db_token = "u8SqwalMUxwyO_AMraGkTWdmp04gbdcboUN3tlEBzMd9xvkPg0cB3MAOsMCwZV-Iy7i2DT_VaLDYjQ-NM_a_LA=="
+bucket = "smart_boiler" 
+org = "smart_household"
+url = "http://influxdb:8086"
+write_client = InfluxDBClient(url=url, token=db_token, org=org)
+# from event_checker import EventChecker
 
-    Returns:
-        [boolean]: [True if DB exists]
-    """
-    dbs = client.get_list_database()
-    for db in dbs:
-        if db['name'] == db_name:
-            return True
-    return False
+# EvntChecker = EventChecker()
 
-def wait_for_server(nretries=5):
-    """Waiting to server response
+# def db_exists():
+#     """Returns True if the database exists.
 
-    Args:
-        host ([string]): [host]
-        port ([int]): [port number]
-        nretries (int, optional): [number of retries]. Defaults to 5.
-    """
-    url = 'http://{}:{}'.format(host, port)
-    waiting_time = 1
-    for i in range(nretries):
-        try:
-            requests.get(url)
-            return 
-        except requests.exceptions.ConnectionError:
-            print('waiting for', url)
-            time.sleep(waiting_time)
-            waiting_time += 2
-            pass
-    print('cannot connect to', url)
+#     Returns:
+#         [boolean]: [True if DB exists]
+#     """
+#     dbs = client.get_list_database()
+#     for db in dbs:
+#         if db['name'] == db_name:
+#             return True
+#     return False
 
-def connect_to_db():
-    global client
-    print('connecting to database: {}:{}'.format(host,port))
-    client = InfluxDBClient(host, port, retries=5, timeout=1)
-    wait_for_server()
-    if not db_exists():
-        print('creating database...')
-        client.create_database(db_name)
-    else:
-        print('database already exists')
-    client.switch_database(db_name)
+# def wait_for_server(nretries=5):
+#     """Waiting to server response
+
+#     Args:
+#         host ([string]): [host]
+#         port ([int]): [port number]
+#         nretries (int, optional): [number of retries]. Defaults to 5.
+#     """
+#     url = 'http://{}:{}'.format(host, port)
+#     waiting_time = 1
+#     for i in range(nretries):
+#         try:
+#             requests.get(url)
+#             return 
+#         except requests.exceptions.ConnectionError:
+#             print('waiting for', url)
+#             time.sleep(waiting_time)
+#             waiting_time += 2
+#             pass
+#     print('cannot connect to', url)
+
+# def connect_to_db():
+#     global client
+#     print('connecting to database: {}:{}'.format(host,port))
+#     client = InfluxDBClient(host, port, retries=5, timeout=1)
+#     wait_for_server()
+#     if not db_exists():
+#         print('creating database...')
+#         client.create_database(db_name)
+#     else:
+#         print('database already exists')
+#     client.switch_database(db_name)
 
 
  
 def measure(bad_request_sleeping_time):
-
+    measurement = SmartBoiler.get_device_status()
     try:
-        http = requests.get("http://" + socket_url + "/status")
-        bad_request_sleeping_time = 10
-    except:
-        print("unable to get request")
-        if(bad_request_sleeping_time != 200):
-            bad_request_sleeping_time +=10
-        time.sleep(bad_request_sleeping_time)
-        return  
-    if http.status_code == 200:
-        try:
-            data = http.json()
+        #client.write_points(data_to_db_json)
+        #####
+        write_api = write_client.write_api(write_options=SYNCHRONOUS)
+        print(write_client)
+        print('creating point')
+        point = Point(measurement)\
+            .tag("shelly_id", measurement['shelly_id'])\
+            .field("power", measurement['power'])\
+            .field("tmp0", measurement['tmp0'])\
+            .field("tmp1", measurement['tmp1'])\
+            .field("ison", measurement['ison'])\
+            .field("in_event", False)\
+            .time(datetime.datetime.utcnow())
 
-            power = data['meters'][0]["power"]
-            temperature_1 = data['ext_temperature']['0']['tC']
-            temperature_2 = data['ext_temperature']['1']['tC']
-            current_time = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
+        print('point created')
+        print(point)
+        print('writing point to db')
 
-            data_to_db_json = [
-                {
-                    "measurement": measurement,
-                    "time": current_time,
-                    
-                    "fields": {
-                        "power": power,
-                        "tmp1": temperature_1, 
-                        "tmp2": temperature_2,
-                        "turned": data['relays'][0]['ison'],
-                        "in_event" : EvntChecker.check_off_event()
-                    }
-                }
-            ]
-
-                    
-        except:
-            print("unable to read data")
-        try:
-            client.write_points(data_to_db_json)
-        except:
-            print("unable to write data in db")
+        write_api.write(bucket=bucket, org=org, record=point)
+        time.sleep(1) # separate points by 1 second
+    #catch except and print out the error
+    except Exception as e:
+        print(e)
+    
+        print("unable to write data in db")
         
     time.sleep(20)
 
@@ -133,13 +125,10 @@ def load_settings(file_name):
 
     
 if __name__ == '__main__':
-    import sys
-    
-    from optparse import OptionParser
+
 
     parser = OptionParser('%prog [OPTIONS] <host> <port>')
 
-  
     parser.add_option(
         '-f', '--settings_file', dest='settings_file',
         type='string', 
@@ -162,8 +151,16 @@ if __name__ == '__main__':
     db_name = settings['db_name']
     measurement = settings['measurement']
 
-    connect_to_db()
+
+
+   
+    #connect_to_db()
     bad_request_sleeping_time = 20
+
+    server_url = "https://shelly-63-eu.shelly.cloud"
+    auth_token = "MTgwMGY3dWlk7121FEEFF87F657946F5BA357BFF57313F2320187ED2CB88C2C46BD89E5E52459B9010E678CFBE44"
+    shelly_device_id = "a4cf12f3f0cd"
+    SmartBoiler = SmartBoiler(server_url, auth_token, shelly_device_id)
 
 
     while(True):
