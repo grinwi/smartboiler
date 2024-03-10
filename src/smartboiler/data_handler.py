e90149295e644e67a81980e8b19d4ef764bf1820
grinwi <grunwald.adam24@gmail.com>
2024-03-10 12:09:32 +0100

    added left and right interval to actual stats

--- src/smartboiler/data_handler.py
+++ src/smartboiler/data_handler.py
@@ -57,9 +57,8 @@ class DataHandler:
         self.last_time_data_update = datetime.now()
         
         
-    def get_actual_boiler_stats(self, group_by_time_interval = "1m", limit = 6):
-        right_time_interval = datetime.now()
-        left_time_interval = right_time_interval - timedelta(minutes=60)
+    def get_actual_boiler_stats(self, group_by_time_interval = "1m", limit = 6, left_time_interval = datetime.now() - timedelta(minutes=60), right_time_interval = datetime.now()):
+
         
         left_time_interval = f"'{left_time_interval.strftime('%Y-%m-%dT%H:%M:%SZ')}'"
         right_time_interval = f"'{right_time_interval.strftime('%Y-%m-%dT%H:%M:%SZ')}'"
