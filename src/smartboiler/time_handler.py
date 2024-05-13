c661073c7555078fd5d8a4ac1097a9250bd03fd1
grinwi <grunwald.adam24@gmail.com>
2024-05-13 09:11:10 +0200

    cleanup time_handled

--- src/smartboiler/time_handler.py
+++ src/smartboiler/time_handler.py
@@ -4,8 +4,9 @@
 #
 # This module is used for manipulating with time and date formats.
 
-from datetime import datetime, timedelta
-from typing import Optional
+from datetime import datetime, timedelta, timezone
+from dateutil import parser
+
 
 
 class TimeHandler:
@@ -15,30 +16,6 @@ class TimeHandler:
         """Initialize the class with the days of the week.
         """
         self.daysofweek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-
-    def float_to_time(self, float_number: float)-> datetime:
-        """Method for converting float number to time format.
-
-        Args:
-            float_number (float): Number to convert.
-
-        Returns:
-            datetime: DateTime object.
-        """
-        return datetime.strptime('{0:02.0f}:{1:02.0f}'.format(*divmod(float_number * 60, 60)), "%H:%M%f")
-
-    def hour_minutes_now(self, date_time: Optional[datetime] = None) -> datetime:
-        """Method for getting actual hour and minute.
-
-        Args:
-            date_time ([DateTime], optional): Datetime object. Defaults to None.
-
-        Returns:
-            [type]: [Hour and minute]
-        """
-        if date_time is None:
-            date_time = datetime.now()
-        return date_time.strptime(date_time.strftime("%H:%M"), "%H:%M")
 
 
     def is_date_between(self, begin_date:datetime, end_date:datetime) -> bool:
@@ -51,22 +28,11 @@ class TimeHandler:
         Returns:
             bool: If the actual date is between the begin and end date returns True, otherwise False.
         """
-        # addition because google calendar api returns utc
-        check_date = datetime.now() + timedelta(hours = 1)
+        check_date = datetime.now(timezone.utc)
 
-        # one hour before the ond of vacation should be water preheated
+        # one hour before the end of vacation should be water preheated
         return begin_date <= check_date <= (end_date - timedelta(hours = 1))
 
-    def date_from_influxdb_to_datetime(self, date_from_db: str) -> datetime:
-        """Method for converting date from InfluxDB to datetime format.
-
-        Args:
-            date_from_db (str): String with date from InfluxDB.
-
-        Returns:
-            datetime: Datetime object of the date.
-        """
-        return datetime.strptime(date_from_db, "%Y-%m-%dT%H:%M:%SZ")
     def date_to_datetime(self, date: str) -> datetime:
         """Method for converting date to datetime format.
 
@@ -76,5 +42,5 @@ class TimeHandler:
         Returns:
             datetime: Datetime object from string date representation.
         """
-        return datetime.strptime(date, "%Y-%m-%dT%H:%M:%S+01:00")
+        return parser.parse(date)
 
